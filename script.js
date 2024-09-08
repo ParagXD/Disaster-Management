9b99c017bc37cbb761a69765137c50a8377c588f
ParagXD <dd231104301013@stu.srhu.edu.in>
2024-09-08 14:37:36 +0530

    added

---
+++ script.js
@@ -0,0 +1,7 @@
+Swal.fire({
+    title: 'Disaster Alert!',
+    text: 'A disaster has been reported in your area. Please take necessary precautions.',
+    icon: 'warning',
+    animation: true,
+    timer: 5000
+})
